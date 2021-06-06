6f67200b68e9c7b781c96f8926222e9f912a95d9
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-06-06 15:56:16 -0300

    Entendendo conceito de fila no clojure

--- src/hospital/core.clj
+++ src/hospital/core.clj
@@ -1,1 +1,7 @@
-(ns hospital.core)
+(ns hospital.core
+  (:use [clojure pprint])
+  (:require [hospital.model :as h.model]))
+
+(let [hospital-do-kindra (h.model/novo-hospital)]
+  (pprint hospital-do-kindra))
+
